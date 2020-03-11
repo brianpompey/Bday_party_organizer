c6f83dd842f641d5269a4c69cb56a36edce4cf8d
Brian Pompey <brianthescholar@gmail.com>
2020-03-11 16:40:28 +0000

    create application controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  end
+
+end
